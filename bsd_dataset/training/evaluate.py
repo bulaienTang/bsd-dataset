d2c6fc0020a8bf69b309a3bcbc2264135f799932
BRYAN(Jingchen) TANG <tangtang1228@ucla.edu>
2022-09-01 19:36:25 -0700

    Update evaluate.py

--- bsd_dataset/training/evaluate.py
+++ bsd_dataset/training/evaluate.py
@@ -31,7 +31,7 @@ def get_metrics(model, dataloader, prefix, options):
                     total_pearsonr += pearsonr(predictions[index], target[index], mask[index])
                     
                     if a==1:
-                        print(total_pearsonr, predictions[index].shape,target[index].shape, mask[index].shape)
+                        print(total_pearsonr, prefix, len(context))
                         a=0
 
         total_rmse /= num_samples
