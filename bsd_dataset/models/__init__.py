e55a4e0cfa12a324aee72c664dfd7dc1228a3156
BRYAN(Jingchen) TANG <tangtang1228@ucla.edu>
2022-09-08 16:49:47 -0700

    fix bug

--- bsd_dataset/models/__init__.py
+++ bsd_dataset/models/__init__.py
@@ -1,3 +1,4 @@
 from .convnet import *
+from .baseline import *
 from .perceiver_io import *
 from .transformer import *
--- bsd_dataset/training/model.py
+++ bsd_dataset/training/model.py
@@ -2,7 +2,7 @@ from ..models import *
 
 def load(model, **kwargs):
     if(model == "LinReg"):
-        return LinReg(input_shape = kwargs["input_shape"], target_shape = kwargs["target_shape"])
+        return linearRegression(input_shape = kwargs["input_shape"], target_shape = kwargs["target_shape"])
     if(model == "AvgPool"):
         return AvgPool(input_shape = kwargs["input_shape"], target_shape = kwargs["target_shape"])
     if(model == "SRCNN"):
