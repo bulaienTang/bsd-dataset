94b48130ca84bf042661e4a49f0bf34300526cab
BRYAN(Jingchen) TANG <tangtang1228@ucla.edu>
2022-09-09 01:32:11 -0700

    Update linear.py

--- bsd_dataset/models/baseline/linear.py
+++ bsd_dataset/models/baseline/linear.py
@@ -8,14 +8,19 @@ __all__ = [
 ]
 
 class linearRegression(nn.Module):
-    def __init__(self, input_shape: List[int], target_shape: List[int]):
+    def __init__(self, input_shape: List[int], target_shape: List[int]): 
+        #[1, 3, 18] [16, 100]
         super(linearRegression, self).__init__()
+        self.w = torch.randn(size = (input_shape[1]*input_shape[2], target_shape[1]*target_shape[2]))
+        self.b = torch.randn(size = (target_shape[1]*target_shape[2],1))
         self.input_shape = input_shape
         self.target_shape = target_shape
-
-        print(input_shape, target_shape)
-        self.linear = nn.Linear(self.input_shape[1], self.target_shape[1])
+        
+        # self.linear = nn.Linear(input_shape[1], target_shape[1])
 
     def forward(self, x):
-        x = self.linear(x)
-        return x.squeeze(1)
+        x = x.squeeze(1)
+        x = x.view(1, x.shape[0] * x.shape[1])
+        pred = x @ self.w + self.b
+        pred = pred.view(self.target_shape[0], self.target_shape[1])
+        return pred
